8d5a7244925ef2a94789bd21ebaa59773c8b4ec2
Peter Clifton Geswint <115699568+peterGeswint@users.noreply.github.com>
2025-06-17 12:42:24 +0200

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import noteRoutes from "./routes/noteRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -17,11 +18,24 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 app.use(express.json()); // Middleware to parse JSON bodies
-app.use(cors({origin: "http://localhost:5173",}));
+
+if(process.env.NODE_ENV !== "production"){
+	app.use(cors({origin: "http://localhost:5173",}));
+};
+
 app.use(rateLimiter);
 app.use("/api/notes", noteRoutes);
+
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*",(req,res) => {
+	res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+});
+}
 
 
 connectDB().then(() => {
